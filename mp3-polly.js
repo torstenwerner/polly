9468b24abedb19b754949e48fb282a01198c5598
Torsten Werner <torsten.werner@westernacher.com>
2016-12-30 23:01:58 +0100

    support docker

--- mp3-polly.js
+++ mp3-polly.js
@@ -1,7 +1,9 @@
 const aws = require('aws-sdk');
 
 const polly = new aws.Polly({
-    region: 'eu-west-1'
+    region: 'eu-west-1',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 module.exports = function (text, voice, type, callback) {
