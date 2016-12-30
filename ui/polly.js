3406d67a9d00fc46035c7e3d61f97f4e813241cb
Torsten Werner <torsten.werner@westernacher.com>
2016-12-30 22:36:49 +0100

    improve form

--- ui/polly.js
+++ ui/polly.js
@@ -1,7 +1,8 @@
 function submitForm(event) {
     const text = document.getElementById('text').value;
+    const voice = document.querySelector('input[name="voice"]:checked').value;
     new Howl({
-        src: [`/audio?text=${text}`],
+        src: [`/audio?text=${text}&voice=${voice}`],
         format: ['mp3'],
         autoplay: true
     });
